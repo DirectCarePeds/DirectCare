f9a5dbd57e6512b805f78d89647514dde8a849f2
Direct Care <157420971+DirectCarePeds@users.noreply.github.com>
2025-09-19 06:25:51 -0600

    Aaron's Massive Update
    Lets Hope I don't break anything.

--- load-footer.js
+++ load-footer.js
@@ -1,6 +1,17 @@
-fetch("footer.html").then(response => response.text()).then(data => {
+// Old version
+// fetch("footer.html").then(response => response.text()).then(data => {
+//     document.getElementById("footer-placeholder").innerHTML = data;
+// })
+
+// Adjust path to your actual file location
+fetch(`footer.html`)
+  .then(response => response.text())
+  .then(data => {
     document.getElementById("footer-placeholder").innerHTML = data;
-})
+  })
+  .catch(error => {
+    console.error("Error loading footer:", error);
+});
 
 // Add the following two lines where you want the footer to be in each file
 // <div id="footer-placeholder"></div>
--- load-nav-bar.js
+++ load-nav-bar.js
@@ -1,6 +1,17 @@
-fetch("nav-bar.html").then(response => response.text()).then(data => {
+// Old version
+// fetch("nav-bar.html").then(response => response.text()).then(data => {
+//     document.getElementById("nav-bar-placeholder").innerHTML = data;
+// })
+
+// Adjust path to your actual file location
+fetch(`nav-bar.html`)
+  .then(response => response.text())
+  .then(data => {
     document.getElementById("nav-bar-placeholder").innerHTML = data;
-})
+  })
+  .catch(error => {
+    console.error("Error loading footer:", error);
+});
 
 // Add the following two lines where you want the footer to be in each file
 // <div id="nav-bar-placeholder"></div>
